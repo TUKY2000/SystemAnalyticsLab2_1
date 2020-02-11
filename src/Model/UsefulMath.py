aa9cc4bdc097fe3086e28eb31a2ddeec882c211b
TUKY2000 <onar2000@gmail.com>
2020-02-11 16:56:48 +0200

    add Runge-Kutta method 4th order

---
+++ src/Model/UsefulMath.py
@@ -0,0 +1,22 @@
+def RungeKutta4(func, x0, y0, steplen, stepcount):
+    def f1(x, y):
+        return func(x, y)
+
+    def f2(x, y):
+        return func(x + 0.5*steplen, y + 0.5*f1)
+
+    def f3(x, y):
+        return func(x + 0.5*steplen, y + 0.5*f2)
+
+    def f4(x, y):
+        return func(x + steplen, y + f3)
+
+    def __next(x, y):
+        return y + h * (f1(x,y) + 2*f2(x,y) + 2*f3(x,y) + f4(x,y)) / 6
+    
+    X = [x0]
+    Y = [y0]
+    for i in range(stepcount):
+        X.append(X[i] + steplen)
+        Y.append(__next(func, X[i], Y[i], steplen))
+    return X, Y
