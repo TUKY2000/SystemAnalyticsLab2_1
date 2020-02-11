84ec0a7bf91dc8a1c0963b59fa847e4c7db798e3
TUKY2000 <onar2000@gmail.com>
2020-02-11 18:01:41 +0200

    add class FullOrderObserver

---
+++ src/Model/SystemStateObserver.py
@@ -0,0 +1,20 @@
+import LinStatSystem as lss
+import numpy as np
+import UsefulMath as usm
+
+class FullOrderObserver:
+    '''
+    Generates estimation of the system states
+    '''
+
+    def __init__(self, system : lss.LinStatSystem):
+        self.system = system
+
+    def estimate(self, managment : list, sysreaction : list, mK : np.matrix, time):
+        return usm.RungeKutta4(self.__dxdt, self.system.x0, 0, steplen = 100, time)
+
+    def __dxdt(x, t):
+        return np.dot(self.system.mA, x) 
+        + np.dot(self.system.mB, self.u) 
+        + np.dot(self.mK,(self.y - np.dot(self.system.mC, x)))
+
