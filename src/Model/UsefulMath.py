fb97786023a60e2e41ef635798a6742e8e22b45f
tukY Chyvekshyno <onar2000@gmail.com>
2020-03-15 19:39:47 +0200

    some fix to Runge Kutta method

--- src/Model/UsefulMath.py
+++ src/Model/UsefulMath.py
@@ -1,4 +1,7 @@
-def RungeKutta4(func, x0, y0, steplen, x1):
+def RungeKutta4(func, x0, y0, x1, steplen):
+    '''
+    solve differencial equation
+    '''
     def f1(x, y):
         return func(x, y)
 
@@ -12,12 +15,12 @@ def RungeKutta4(func, x0, y0, steplen, x1):
         return func(x + steplen, y + f3)
 
     def __next(x, y):
-        return y + h * (f1(x,y) + 2*f2(x,y) + 2*f3(x,y) + f4(x,y)) / 6
+        return y + steplen * (f1(x,y) + 2*f2(x,y) + 2*f3(x,y) + f4(x,y)) / 6
     
     X = [x0]
     Y = [y0]
     stepcount = x1 / steplen
     for i in range(stepcount):
         X.append(X[i] + steplen)
-        Y.append(__next(func, X[i], Y[i], steplen))
+        Y.append(__next(X[i], Y[i]))
     return X, Y
